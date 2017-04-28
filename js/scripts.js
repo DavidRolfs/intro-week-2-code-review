e6bcb754931a2e5609f5d78b7dce4763c42770c9
David Rolfs <Davidrolfs15@gmail.com>
2017-04-28 09:19:21 -0700

    assign user input answer to variables using jQuery selectors, test using alert

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
   $("form#track").submit(function(){
-    alert("works");
+    // alert("works");
+    var q1 = $("#question1").val();
+    var q2 = $("#question2").val();
+    var q3 = $("#question3").val();
+    var q4 = $("#question4").val();
+    var q5 = $("#question5").val()
+    //alert(q1 + q2 + q3 + q4 + q5);
+
+
+
   });
 });
